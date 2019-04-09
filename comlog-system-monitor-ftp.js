9713fabb28721ddbcdf75b6f4dc0c0f49dc0acea
ar <a.rau@comlog.org>
2019-04-09 10:57:34 +0200

    ~ Custom Logger

--- comlog-system-monitor-ftp.js
+++ comlog-system-monitor-ftp.js
@@ -7,6 +7,8 @@ function ComlogFTPWatcher(options) {
 	this.satus = null; // null = start, true = off, false = on
 	this.debug = false;
 	this.interval = 60000; // 1 Minute
+	this.logger = console;
+
 
 	// Private funktionen
 	var _running = false, _timer = null;
@@ -31,7 +33,7 @@ function ComlogFTPWatcher(options) {
 		var c = new Client();
 		var __event = function(err) {
 			if (err !== null) {
-				if (_self.debug) console.error(err.stack || err);
+				if (_self.debug) _self.logger.error(err.stack || err);
 				_self.emit('error', [new Error("Connection to \""+options.user+'@'+options.host+"\" filed \n"+err.message)]);
 				if (_self.satus === true) _self.emit('down');
 				_self.satus = false;
@@ -40,7 +42,7 @@ function ComlogFTPWatcher(options) {
 				var new_status = true, msg = "Connection to \""+options.user+'@'+options.host+"\" check ok \n";
 
 				// check result
-				if (_self.debug) console.info(msg);
+				if (_self.debug) _self.logger.info(msg);
 				if (_self.satus !== null && _self.satus !== new_status) _self.emit(new_status ? 'up' : 'down');
 				_self.satus = new_status;
 
